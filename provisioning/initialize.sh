8a461655512b58e6901b5180b1ee041a3c1627cf
Evan Jackson <evan@jacksonnet.ca>
2023-02-15 20:12:52 -0400

    Now accepting a service principal name in initialize.sh and adding some helpful printouts

--- provisioning/initialize.sh
+++ provisioning/initialize.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+echo Logging into tenant $TENANT_NAME...
 az login --tenant $TENANT_NAME
+echo Creating resource group $RESOURCE_GROUP in $LOCATION...
 az group create --location $LOCATION --name $RESOURCE_GROUP
-az ad sp create-for-rbac --role Contributor --scopes subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP
+echo Creating service principal $SERVICE_PRINCIPAL_NAME...
+az ad sp create-for-rbac \
+    --role Contributor \
+    --scopes subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP \
+    --display-name $SERVICE_PRINCIPAL_NAME
+echo Logging out of Azure...
 az logout
