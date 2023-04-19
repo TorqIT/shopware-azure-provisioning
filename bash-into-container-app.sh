650df41ac8078bc6cd5287426fbd3d3611643f74
Evan Jackson <evan@jacksonnet.ca>
2023-04-19 11:30:34 -0300

    Adding helper script for bash'ing into the PHP-FPM container app

---
+++ bash-into-container-app.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' $1)
+PHP_FPM_CONTAINER_APP=$(jq -r '.parameters.phpFpmContainerAppName.value' $1)
+
+az containerapp exec --resource-group $RESOURCE_GROUP --name $PHP_FPM_CONTAINER_APP bash
