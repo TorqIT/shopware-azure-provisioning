eb80bb0ac1741626fc0c8e08ff987284f3dcdb19
Evan Jackson <evan@jacksonnet.ca>
2023-05-10 13:28:05 -0400

    Typo

--- initialize-resource-group-and-service-principal.sh
+++ initialize-resource-group-and-service-principal.sh
@@ -5,7 +5,7 @@ set -e
 export SUBSCRIPTION_ID=$(jq -r '.parameters.subscriptionId.value' $1)
 export RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' $1)
 export LOCATION=$(jq -r '.parameters.location.value' $1)
-export $SERVICE_PRINCIPAL_NAME=$(jq -r '.parameters.servicePrincipalName.value' $1)
+export SERVICE_PRINCIPAL_NAME=$(jq -r '.parameters.servicePrincipalName.value' $1)
 
 echo Creating resource group $RESOURCE_GROUP in $LOCATION...
 az group create --location $LOCATION --name $RESOURCE_GROUP
