1f72aa28153143d3c51f466fbeb5d34bb9936e4e
Evan Jackson <evan.jackson@torqit.ca>
2023-08-04 15:12:54 -0300

    Setting subscription when logging in

--- login-to-tenant.sh
+++ login-to-tenant.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 TENANT_NAME=$(jq -r '.parameters.tenantName.value' $1)
+SUBSCRIPTION_ID=$(jq -r '.parameters.subscriptionId.value' $1)
 
 echo Logging in to Azure tenant $TENANT_NAME...
 az login --tenant $TENANT_NAME
+az account set --subscription $SUBSCRIPTION_ID
