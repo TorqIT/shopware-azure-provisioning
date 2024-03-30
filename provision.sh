7f10eb497a443513165cb5efdfa17b64558df059
Evan Jackson <evan.jackson@torqit.ca>
2024-03-30 19:53:19 -0400

    More complete support for existing Key Vault

--- provision.sh
+++ provision.sh
@@ -6,14 +6,15 @@ RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' $1)
 
 KEY_VAULT_NAME=$(jq -r '.parameters.keyVaultName.value' $1)
 KEY_VAULT_RESOURCE_GROUP_NAME=$(jq -r '.parameters.keyVaultResourceGroupName.value' $1)
-echo "Deploying Key Vault..."
-az deployment group create \
-  --resource-group $KEY_VAULT_RESOURCE_GROUP_NAME \
-  --template-file ./bicep/key-vault/key-vault.bicep \
-  --parameters \
-    name=$KEY_VAULT_NAME \
-    localIpAddress=$(curl ipinfo.io/ip)
-
+if [ "${KEY_VAULT_RESOURCE_GROUP_NAME:-$RESOURCE_GROUP}" == "${RESOURCE_GROUP}" ]; then
+  echo "Deploying Key Vault..."
+  az deployment group create \
+    --resource-group $RESOURCE_GROUP \
+    --template-file ./bicep/key-vault/key-vault.bicep \
+    --parameters \
+      name=$KEY_VAULT_NAME \
+      localIpAddress=$(curl ipinfo.io/ip)
+fi
 WAIT_FOR_KEY_VAULT_MANUAL_INTERVENTION=$(jq -r '.parameters.waitForKeyVaultManualIntervention.value' $1)
 if [ "${WAIT_FOR_KEY_VAULT_MANUAL_INTERVENTION:-false}" = true ]; then
   read -p "Use the Azure Portal to update the Key Vault's access policies (e.g. give yourself the ability to add secrets), and to add any keys/secrets needed for the rest of the resources (e.g. a database password). Then, press Enter to continue... "
@@ -25,12 +26,15 @@ fi
 echo "Deploying Container Registry..."
 CONTAINER_REGISTRY_NAME=$(jq -r '.parameters.containerRegistryName.value' $1)
 CONTAINER_REGISTRY_SKU=$(jq -r '.parameters.containerRegistrySku.value' $1)
-az deployment group create \
-  --resource-group $RESOURCE_GROUP \
-  --template-file ./bicep/container-registry/container-registry.bicep \
-  --parameters \
-    containerRegistryName=$CONTAINER_REGISTRY_NAME \
-    sku=$CONTAINER_REGISTRY_SKU
+CONTAINER_REGISTRY_RESOURCE_GROUP_NAME=$(jq -r '.parameters.containerRegistryResourceGroupName.value' $1)
+if [ "${CONTAINER_REGISTRY_RESOURCE_GROUP_NAME:-$RESOURCE_GROUP}" == "${RESOURCE_GROUP}" ]; then
+  az deployment group create \
+    --resource-group $RESOURCE_GROUP \
+    --template-file ./bicep/container-registry/container-registry.bicep \
+    --parameters \
+      containerRegistryName=$CONTAINER_REGISTRY_NAME \
+      sku=$CONTAINER_REGISTRY_SKU
+fi
 ./bicep/container-registry/deploy-images.sh $1
 ./bicep/container-registry/purge-container-registry-task.sh $1
 
