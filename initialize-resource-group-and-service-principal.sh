cb6bd48befd91891fac30061a50e4d6e2f7e9aa1
Evan Jackson <evan.jackson@torqit.ca>
2024-06-25 10:18:03 -0400

    Service principal script

--- initialize-resource-group-and-service-principal.sh
+++ initialize-resource-group-and-service-principal.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-export SUBSCRIPTION_ID=$(jq -r '.parameters.subscriptionId.value' $1)
-export RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' $1)
-export LOCATION=$(jq -r '.parameters.location.value' $1)
-export SERVICE_PRINCIPAL_NAME=$(jq -r '.parameters.servicePrincipalName.value' $1)
+SUBSCRIPTION_ID=$(jq -r '.parameters.subscriptionId.value' $1)
+RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' $1)
+LOCATION=$(jq -r '.parameters.location.value' $1)
+SERVICE_PRINCIPAL_NAME=$(jq -r '.parameters.servicePrincipalName.value' $1)
+CONTAINER_REGISTRY_NAME=$(jq -r '.parameters.containerRegistryName.value' $1)
+SHOPWARE_CONTAINER_APP_NAME=$(jq -r '.parameters.shopwareContainerAppName.value' $1)
+INIT_CONTAINER_APP_JOB_NAME=$(jq -r '.parameters.initContainerAppJobName.value' $1)
 
 echo Creating resource group $RESOURCE_GROUP in $LOCATION...
 az group create --location $LOCATION --name $RESOURCE_GROUP
@@ -13,7 +16,22 @@ az group create --location $LOCATION --name $RESOURCE_GROUP
 echo Creating service principal $SERVICE_PRINCIPAL_NAME...
 az ad sp create-for-rbac --display-name $SERVICE_PRINCIPAL_NAME
 
+SERVICE_PRINCIPAL_ID=$(az ad sp list --display-name korth-shopware-sp-dev --query "[].{spID:appId}" --output tsv)
+
+echo Assigning AcrPush role on Container Registry to service principal...
 az role assignment create \
-    --assignee "{assignee}" \
-    --role "{roleNameOrId}" \
-    --scope "/subscriptions/{subscriptionId}/resourcegroups/{resourceGroupName}/providers/{providerName}/{resourceType}/{resourceSubType}/{resourceName}"
+    --assignee $SERVICE_PRINCIPAL_ID \
+    --role "AcrPush" \
+    --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.ContainerRegistry/registries/$CONTAINER_REGISTRY_NAME"
+
+echo Assigning Contributor role on Shopware Container App to service principal...
+az role assignment create \
+    --assignee $SERVICE_PRINCIPAL_ID \
+    --role "AcrPush" \
+    --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.App/containerapps/$SHOPWARE_CONTAINER_APP_NAME"
+
+echo Assigning Contributor role on init Container App Job to service principal...
+az role assignment create \
+    --assignee $SERVICE_PRINCIPAL_ID \
+    --role "AcrPush" \
+    --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.App/jobs/$INIT_CONTAINER_APP_JOB_NAME"
