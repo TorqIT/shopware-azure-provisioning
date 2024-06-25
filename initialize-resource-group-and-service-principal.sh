4935bcaaed90d723b10857cadc3058a3aa2b0c6a
Evan Jackson <evan.jackson@torqit.ca>
2024-06-25 10:43:50 -0400

    Fixing role assignment for SP on CAs

--- initialize-resource-group-and-service-principal.sh
+++ initialize-resource-group-and-service-principal.sh
@@ -27,11 +27,11 @@ az role assignment create \
 echo Assigning Contributor role on Shopware Container App to service principal...
 az role assignment create \
     --assignee $SERVICE_PRINCIPAL_ID \
-    --role "AcrPush" \
+    --role "Contributor" \
     --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.App/containerapps/$SHOPWARE_CONTAINER_APP_NAME"
 
 echo Assigning Contributor role on init Container App Job to service principal...
 az role assignment create \
     --assignee $SERVICE_PRINCIPAL_ID \
-    --role "AcrPush" \
+    --role "Contributor" \
     --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.App/jobs/$INIT_CONTAINER_APP_JOB_NAME"
