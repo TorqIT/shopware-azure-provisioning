8e8b67cac0e0004eb911cbb3daad10aab49f2aa4
Evan Jackson <evan.jackson@torqit.ca>
2024-07-11 14:32:16 -0400

    Disabling subscription selector on login

--- login-to-tenant.sh
+++ login-to-tenant.sh
@@ -4,5 +4,7 @@ TENANT_NAME=$(jq -r '.parameters.tenantName.value' $1)
 SUBSCRIPTION_ID=$(jq -r '.parameters.subscriptionId.value' $1)
 
 echo Logging in to Azure tenant $TENANT_NAME...
+# Disable interactive subscription selector since we set the default subscription below
+az config set core.login_experience_v2=off
 az login --tenant $TENANT_NAME
 az account set --subscription $SUBSCRIPTION_ID
