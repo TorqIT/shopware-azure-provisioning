24f3c19a4fdb3c9134dd9aeb2c23ee7414b670f0
Evan Jackson <evan.jackson@torqit.ca>
2024-07-24 10:54:27 -0300

    Typo

--- initialize-resource-group-and-service-principal.sh
+++ initialize-resource-group-and-service-principal.sh
@@ -25,7 +25,7 @@ az role assignment create \
     --role "AcrPush" \
     --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.ContainerRegistry/registries/$CONTAINER_REGISTRY_NAME"
 
-echo Assigning Contributor role on Shopware Container App to service principal...
+echo Assigning Contributor role on PHP Container App to service principal...
 az role assignment create \
     --assignee $SERVICE_PRINCIPAL_ID \
     --role "Contributor" \
