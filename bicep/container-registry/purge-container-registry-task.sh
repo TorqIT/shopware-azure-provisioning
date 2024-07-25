624c44bdbdee1f3583401111cb7af55eb4612673
Evan Jackson <evan.jackson@torqit.ca>
2024-07-25 09:26:46 -0300

    Purging init image from Container Registry

--- bicep/container-registry/purge-container-registry-task.sh
+++ bicep/container-registry/purge-container-registry-task.sh
@@ -6,11 +6,16 @@ echo Setting up scheduled task to purge all but the latest 10 containers...
 
 RESOURCE_GROUP=$(jq -r '.parameters.resourceGroupName.value' $1)
 CONTAINER_REGISTRY_NAME=$(jq -r '.parameters.containerRegistryName.value' $1)
+INIT_IMAGE_NAME=$(jq -r '.parameters.initImageName.value // empty' $1)
 PHP_FPM_IMAGE_NAME=$(jq -r '.parameters.phpFpmImageName.value' $1)
 SUPERVISORD_IMAGE_NAME=$(jq -r '.parameters.supervisordImageName.value' $1)
 REDIS_IMAGE_NAME=$(jq -r '.parameters.redisImageName.value' $1)
 
 CONTAINER_REGISTRY_REPOSITORIES=($PHP_FPM_IMAGE_NAME $SUPERVISORD_IMAGE_NAME $REDIS_IMAGE_NAME)
+if [ ! -z "${INIT_IMAGE_NAME}" ];
+then
+  CONTAINER_REGISTRY_REPOSITORIES+=($INIT_IMAGE_NAME)
+fi
 
 PURGE_CMD="acr purge "
 for repository in ${CONTAINER_REGISTRY_REPOSITORIES[@]}
