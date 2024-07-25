404fb89231068aef03f11e5e23b60ba43bf7fda1
Evan Jackson <evan.jackson@torqit.ca>
2024-07-25 13:36:37 -0300

    Now simply deploying Hello World images if no Container Registry repositories are present

--- bicep/container-registry/deploy-images.sh
+++ bicep/container-registry/deploy-images.sh
@@ -2,29 +2,31 @@
 
 set -e
 
-DEPLOY_IMAGES_TO_CONTAINER_REGISTRY=$(jq -r '.parameters.deployImagesToContainerRegistry.value' $1)
 CONTAINER_REGISTRY_NAME=$(jq -r '.parameters.containerRegistryName.value' $1)
 INIT_IMAGE_NAME=$(jq -r '.parameters.initImageName.value // empty' $1)
 PHP_FPM_IMAGE_NAME=$(jq -r '.parameters.phpFpmImageName.value' $1)
 SUPERVISORD_IMAGE_NAME=$(jq -r '.parameters.supervisordImageName.value' $1)
 REDIS_IMAGE_NAME=$(jq -r '.parameters.redisImageName.value' $1)
 
-if $DEPLOY_IMAGES_TO_CONTAINER_REGISTRY
+$IMAGES=( $PHP_FPM_IMAGE_NAME $SUPERVISORD_IMAGE_NAME $REDIS_IMAGE_NAME )
+if [ ! -z $INIT_IMAGE_NAME ];
+then
+  $IMAGES+=($INIT_IMAGE_NAME)
+fi
+
+EXISTING_REPOSITORIES=$(az acr repository list --name $CONTAINER_REGISTRY_NAME)
+
+if [ ${#EXISTING_REPOSITORIES[@]} -eq 0 ];
 then
   # Container Apps require images to actually be present in the Container Registry in order to complete provisioning,
-  # therefore we tag and push them here. Note that this assumes the images are already built and available, and depends
-  # on the local image names being defined as environment variables (see README).
-  echo Pushing images to Container Registry...
+  # therefore we tag and push some dummy Hello World ones here. 
+  echo Pushing Hello World images to Container Registry...
+  docker pull hello-world
   az acr login --name $CONTAINER_REGISTRY_NAME
-  declare -A IMAGES=( [$LOCAL_PHP_FPM_IMAGE]=$PHP_FPM_IMAGE_NAME [$LOCAL_SUPERVISORD_IMAGE]=$SUPERVISORD_IMAGE_NAME [$LOCAL_REDIS_IMAGE]=$REDIS_IMAGE_NAME )
-  if [ ! -z "${INIT_IMAGE_NAME}" ];
-  then
-    IMAGES+=( [$LOCAL_INIT_IMAGE]=$INIT_IMAGE_NAME )
-  fi
-  for image in "${!IMAGES[@]}"
+  for image in "${IMAGES[@]}"
   do
-    docker tag $image $CONTAINER_REGISTRY_NAME.azurecr.io/${IMAGES[$image]}:latest
-    docker push $CONTAINER_REGISTRY_NAME.azurecr.io/${IMAGES[$image]}:latest
+    docker tag hello-world:latest $CONTAINER_REGISTRY_NAME.azurecr.io/$image:latest
+    docker push $CONTAINER_REGISTRY_NAME.azurecr.io/$image:latest
   done
   docker logout
 fi
