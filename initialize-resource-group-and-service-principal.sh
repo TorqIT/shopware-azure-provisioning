b9b228d60022d943e49e1b51e9bcafe05a65abdd
Evan Jackson <evan.jackson@torqit.ca>
2024-08-23 18:09:47 -0400

    Typo

--- initialize-resource-group-and-service-principal.sh
+++ initialize-resource-group-and-service-principal.sh
@@ -45,7 +45,7 @@ echo Assigning Contributor role on supervisord Container App to service principa
 az role assignment create \
     --assignee $SERVICE_PRINCIPAL_ID \
     --role "Contributor" \
-    --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.App/containerapps/$SUPERVISORD_CONTAINER_APP"
+    --scope "/subscriptions/$SUBSCRIPTION_ID/resourcegroups/$RESOURCE_GROUP/providers/Microsoft.App/containerapps/$SUPERVISORD_CONTAINER_APP_NAME"
 
 echo Assigning Key Vault Secrets User on Key Vault to service principal...
 az role assignment create \
