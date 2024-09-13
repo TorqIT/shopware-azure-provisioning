40441966540307a25cd905b535777bf2c8a369ca
Evan Jackson <evan.jackson@torqit.ca>
2024-09-12 20:37:41 -0400

    Proper handling of provisionServicePrincipal falsey values

--- provision.sh
+++ provision.sh
@@ -51,8 +51,8 @@ az deployment group create \
   --template-file ./bicep/main.bicep \
   --parameters @$1
 
-PROVISION_SERVICE_PRINCIPAL=$(jq -r '.parameters.provisionServicePrincipal.value // empty' $1)
-if [ "${PROVISION_SERVICE_PRINCIPAL:-true}" = true ]
+PROVISION_SERVICE_PRINCIPAL=$(jq -r '.parameters.provisionServicePrincipal.value' $1) #alternative operator (//) does not work here because "false" makes it always execute
+if [ "${PROVISION_SERVICE_PRINCIPAL}" = "null" ] || [ "${PROVISION_SERVICE_PRINCIPAL}" = true ]
 then
   SERVICE_PRINCIPAL_NAME=$(jq -r '.parameters.servicePrincipalName.value' $1)
   SERVICE_PRINCIPAL_ID=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].{spID:id}" --output tsv)
